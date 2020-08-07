7a0ed04dbf65b43f0943da7f628aef3316d19660
Brandon <9inchsamurai@gmail.com>
2020-08-07 13:19:53 -0600

    start component for SG mission reward table

---
+++ src/components/SG/MissionRewardTable.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+export declare namespace MissionRewardTable {
+  export interface MissionInfo {
+    region: string;
+    city: string;
+    name: string;
+    duration: number;
+    stat: string;
+    ability_increase: string;
+    basic_reward: string;
+    great_reward: string;
+  }
+
+  export interface Props {
+    missionData: MissionInfo[];
+  }
+}
+
+function renderMissionInfo(data: MissionRewardTable.MissionInfo[]) {
+  return data.map((mission) => {
+    return (
+      <tr>
+        <td>{mission.region}</td>
+        <td>{mission.city}</td>
+        <td>{mission.stat}</td>
+        <td>{mission.basic_reward}</td>
+        <td>{mission.great_reward}</td>
+        <td>{mission.ability_increase}</td>
+        <td>{mission.duration}</td>
+      </tr>
+    );
+  });
+}
+
+export function MissionRewardTable(props: MissionRewardTable.Props) {
+  const { missionData } = props;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>Region</td>
+          <td>City</td>
+          <td>Attribute Requirement</td>
+          <td>Basic Reward</td>
+          <td>Great Reward</td>
+          <td>Ability Increase</td>
+          <td>Duration</td>
+        </tr>
+      </thead>
+      <tbody>{renderMissionInfo(missionData)}</tbody>
+    </table>
+  );
+}
