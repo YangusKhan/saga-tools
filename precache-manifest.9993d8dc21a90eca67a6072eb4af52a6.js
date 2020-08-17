529f8fe5cc48ad1461db1b3d8d11f24533554226
Brandon <9inchsamurai@gmail.com>
2020-08-17 14:30:01 -0600

    Updates

---
+++ precache-manifest.9993d8dc21a90eca67a6072eb4af52a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bc0f09421494708d7ad5c9b3fccee5",
+    "url": "/saga-tools/index.html"
+  },
+  {
+    "revision": "101b28762f7199ac6ed7",
+    "url": "/saga-tools/static/css/main.b28cbaef.chunk.css"
+  },
+  {
+    "revision": "cec6b5ac49490086c367",
+    "url": "/saga-tools/static/js/2.7687119f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saga-tools/static/js/2.7687119f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101b28762f7199ac6ed7",
+    "url": "/saga-tools/static/js/main.8efbf78f.chunk.js"
+  },
+  {
+    "revision": "0a33d0da15b9b4d02dbf",
+    "url": "/saga-tools/static/js/runtime-main.3f54c720.js"
+  }
+]);
